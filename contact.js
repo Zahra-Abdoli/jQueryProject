6dd0778763f2d3e15c5e35f158a17e273d80e63a
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-06-23 14:06:06 +0200

    ew

--- contact.js
+++ contact.js
@@ -1,7 +1,7 @@
 function initMap() {
     // The location of Uluru
     var uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
+    // The map, centered at Uluruf
     var map = new google.maps.Map(
         document.getElementById('map'), { zoom: 4, center: uluru });
     // The marker, positioned at Uluru
